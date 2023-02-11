e597d57c26a10da641d90e26d65841c2bff6c7fa
Jaap Weijland <jaapweijland@gmail.com>
2023-02-11 16:38:59 +0100

    refactor: designed homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import 'blog/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google';
+import 'blog/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <style jsx global>{`
+                html {
+                    font-family: ${inter.style.fontFamily};
+                }
+            `}</style>
+            <Component {...pageProps} />
+        </>
+    );
 }
